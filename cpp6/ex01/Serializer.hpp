cd5f22811119579b9c262e73e0369ffc0fbbcc96
Soufiane Ait naceur <sait-nac@e2r11p2.1337.ma>
2025-09-20 18:09:31 +0100

    fixing some compilation bugs in the ex01

--- cpp6/ex01/Serializer.hpp
+++ cpp6/ex01/Serializer.hpp
@@ -1,10 +1,12 @@
 #ifndef SERIALIZER_HPP
 #define SERIALIZER_HPP
 #include "Data.hpp"
+#include <stdint.h>
+
 class Serializer {
 public:
   static uintptr_t serialize(Data *ptr);
-  Data *deserialize(uintptr_t raw);
+  static Data *deserialize(uintptr_t raw);
 
 private:
   Serializer();
