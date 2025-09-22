5d4407aa439a4a694b152f250a157a2a07c14e40
Soufiane Ait naceur <sait-nac@e2r11p2.1337.ma>
2025-09-22 12:18:25 +0100

    last checked

--- cpp7/ex01/main.cpp
+++ cpp7/ex01/main.cpp
@@ -3,7 +3,7 @@
 template <typename T> void printing(T x) { std::cout << x << std::endl; }
 int main(void) {
   int numbers[] = {1, 2, 3, 4, 5};
-  const char string[] = "ahmed";
+  char string[] = "ahmed";
   size_t len = sizeof(numbers) / sizeof(numbers[0]);
   iter(string, len, printing<char>);
 }
