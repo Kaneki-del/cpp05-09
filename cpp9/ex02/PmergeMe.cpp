2ff220a92f11346a1bfbc83462718c5341e59872
Soufiane Ait naceur <sait-nac@e2r6p14.1337.ma>
2025-09-30 13:05:17 +0100

    implementing the function that parce and store the input

--- cpp9/ex02/PmergeMe.cpp
+++ cpp9/ex02/PmergeMe.cpp
@@ -3,26 +3,70 @@
 
 PmergeMe::PmergeMe() {}
 PmergeMe::~PmergeMe() {}
-PmergeMe::PmergeMe(const PmergeMe &other) {}
-PmergeMe &PmergeMe::operator=(const PmergeMe &other) {}
 
-long parseAndValidate(const std::string &str) {
+PmergeMe::PmergeMe(const PmergeMe &other)
+    : _vectorSequence(other._vectorSequence),
+      _dequeSequence(other._dequeSequence) {}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
+  if (this != &other) {
+    _vectorSequence = other._vectorSequence;
+    _dequeSequence = other._dequeSequence;
+  }
+  return *this;
+}
+
+void PmergeMe::sortVector() {
+  // first check the size if not pair to remove the last element and have it
+  // ther
+  //  sort the pairs that explained from the guide  by swaping theme
+  //  split the the smal one and the largest one in two container
+  //  generate the squenece and try to implement the insertion
+}
+
+long PmergeMe::parseAndValidate(const std::string &str) {
+  const char *nptr = str.c_str();
   char *endptr;
   errno = 0;
-  long amount = std::strtol(str.c_str(), &endptr, 10);
-  if (endptr == str.c_str() || *endptr != '\0' || errno == ERANGE) {
-  }
+  long amount = std::strtol(nptr, &endptr, 10);
 
-  void processSequence(char **av, int count) {
-    std::vector<long> initialSequence;
-    for (int i = 0; i < count; ++i) {
-      long num = parseAndValidate(av[i]);
-      initialSequence.push_back(num);
-    }
+  if (endptr == nptr) {
+    throw std::invalid_argument("Error: Input is not a valid number.");
   }
+  if (*endptr != '\0') {
+    throw std::invalid_argument(
+        "Error: Input contains non-numeric characters.");
+  }
+  if (errno == ERANGE) {
+    throw std::out_of_range(
+        "Error: Number exceeds the system's long integer range.");
+  }
+  if (amount < 1) {
+    throw std::invalid_argument(
+        "Error: Input must be a positive integer (>= 1).");
+  }
+  if (amount > std::numeric_limits<int>::max()) {
+    throw std::out_of_range(
+        "Error: Number exceeds the allowed max integer value (INT_MAX).");
+  }
+  return amount;
+}
+void PmergeMe::processSequence(char **av, int count) {
+  std::vector<long> initialSequence;
+  for (int i = 0; i < count; ++i) {
+    long num = parseAndValidate(av[i]);
+    initialSequence.push_back(num);
+  }
+  displaySequence(initialSequence, "Before: ");
 
-  void displayUsage() {
-    std::cerr << "Error: Usage: ./PmergeMe < positive_integer_sequence > "
-              << std::endl;
-    std::cerr << "Example: ./PmergeMe 3 5 9 7 4 " << std::endl;
-  }
+  _vectorSequence.assign(initialSequence.begin(), initialSequence.end());
+  _dequeSequence.assign(initialSequence.begin(), initialSequence.end());
+
+  sortVector();
+}
+
+void displayUsage() {
+  std::cerr << "Error: Usage: ./PmergeMe < positive_integer_sequence > "
+            << std::endl;
+  std::cerr << "Example: ./PmergeMe 3 5 9 7 4 " << std::endl;
+}
--- cpp9/ex02/PmergeMe.hpp
+++ cpp9/ex02/PmergeMe.hpp
@@ -1,8 +1,11 @@
 #ifndef PMERGEME
 #define PMERGEME
 
+#include <cerrno>
 #include <deque>
 #include <iostream>
+#include <iterator>
+#include <limits>
 #include <vector>
 
 class PmergeMe {
@@ -12,11 +15,24 @@ public:
   PmergeMe(const PmergeMe &other);
   PmergeMe &operator=(const PmergeMe &other);
   void processSequence(char **av, int count);
+  template <typename Container>
+  void displaySequence(const Container &seq, const std::string &prefix) {
+    std::cout << prefix;
+
+    if (!seq.empty()) {
+      std::copy(seq.begin(), seq.end(),
+                std::ostream_iterator<typename Container::value_type>(std::cout,
+                                                                      " "));
+    }
+    std::cout << std::endl;
+  }
 
 private:
   std::vector<long> _vectorSequence;
   std::deque<long> _dequeSequence;
   long parseAndValidate(const std::string &num);
+  void sortVector();
+  void sortDeque();
 };
 void displayUsage();
 #endif
